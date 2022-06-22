f2a81e9dafb1091baec8ed39ffb9b3c98c96e55d
Lancelot S. Oleriana <99029899+GonzoTrickster9899@users.noreply.github.com>
2022-06-22 22:50:02 +0800

    Create Basic #002.c

---
+++ Basic #002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//--------------------------------------------------------------//
+//	Basic #002:				          	//
+//	  Write a C program to get the C version you are using.	//
+//--------------------------------------------------------------//
+
+int main(){                            // Initial program
+	printf("We are using C11!\n"); // Print your version of C language program that you use on the screen
+}                                      // End program
