2095f204df54e4d2a21e57e643e36d3555ce4426
Lancelot S. Oleriana <99029899+GonzoTrickster9899@users.noreply.github.com>
2022-06-22 22:33:20 +0800

    Add files via upload

---
+++ Basic #001/Basic #001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//------------------------------------------------------------------------------//
+//	Basic #001																	//
+//		Write a C program to print your name, date of birth, and mobile number.	//
+//------------------------------------------------------------------------------//
+
+int main(){
+	printf("Name	: Lancelot S. Oleriana\n");
+	printf("DOB	: November 21, 1999\n");
+	printf("Mobile	: 0956 875 7828\n");
+}
