a7e4f0118ab56652fb98690c439044442808bf60
Lancelot S. Oleriana <99029899+GonzoTrickster9899@users.noreply.github.com>
2022-06-22 22:46:16 +0800

    Create Basic #001.c

---
+++ Basic #001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//------------------------------------------------------------------------------//
+//	Basic #001			                                        //
+//	Write a C program to print your name, date of birth, and mobile number.	//
+//------------------------------------------------------------------------------//
+
+int main(){                                          // Initial program
+	printf("Name	  : RK Bagatsing  \n");       // Print a name on the screen
+	printf("DOB	  : May 9, 1988   \n");       // Print a date of birth (DOB) on the screen
+	printf("Mobile	  : 0969 481 4479 \n");       // Print a mobile number on the screen
+}                                                     // End of program
