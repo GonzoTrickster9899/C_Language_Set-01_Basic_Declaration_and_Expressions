3dcd410ece7d7d7292c42c541344b3aff94af730
Lancelot S. Oleriana <99029899+GonzoTrickster9899@users.noreply.github.com>
2022-08-09 23:47:10 +0800

    Update Basic #021.c

--- Basic #021.c
+++ Basic #021.c
@@ -6,19 +6,19 @@
 //    Specified Range: [0,20], [21,40], [41,60], [61,80]                                        //
 //----------------------------------------------------------------------------------------------//
 
-int main(){// Start a program
-	int num;// Declare the integer num
-	printf("Input an integer: ");// Input an integer num
-	scanf("%d", &num);// Read an integer num that you inputted
-	if(num>=0 && num<=20){// IF Statement: If num is greater than or equal to 0 and num is less than or equal to 20
-		printf("\nRange [0,20]\n");// Then, print "Range [0,20]" on the screen 
-	}else if(num>=21 && num<=40){// If num is greater than or equal to 21 and num is less than or equal to 40
-		printf("\nRange (25, 50]\n");// Then, print "Range (25,50]" on the screen
-	}else if(num>=41 && num<=60){// If num is greater than or equal to 41 and num is less than or equal to 60
-		printf("\nRange (50,75]\n");// Then, print "Range (50,75]" on the screen
-	}else if(num>=61 && num<=80){// If num is greater than or equal to 61 and num is less than or equal to 80
-		printf("\nRange (61,80]\n");// Then, print "Range (61,80]" on the screen
-	}else{// If all of these are not on the range
-		printf("Outside the range\n");// Then, print "Outside the range" on the screen
-	}// Close IF Statement
-}// Terminate a program
+int main(){                         // Start a program
+  int num;                          // Declare the integer num
+  printf("Input an integer: ");     // Input an integer num
+  scanf("%d", &num);                // Read an integer num that you inputted
+  if(num>=0 && num<=20){            // IF Statement: If num is greater than or equal to 0 and num is less than or equal to 20
+    printf("\nRange [0,20]\n");     // Then, print "Range [0,20]" on the screen 
+  }else if(num>=21 && num<=40){     // If num is greater than or equal to 21 and num is less than or equal to 40
+    printf("\nRange (25, 50]\n");   // Then, print "Range (25,50]" on the screen
+  }else if(num>=41 && num<=60){     // If num is greater than or equal to 41 and num is less than or equal to 60
+    printf("\nRange (50,75]\n");    // Then, print "Range (50,75]" on the screen
+  }else if(num>=61 && num<=80){     // If num is greater than or equal to 61 and num is less than or equal to 80
+    printf("\nRange (61,80]\n");    // Then, print "Range (61,80]" on the screen
+  }else{                            // If all of these are not on the range
+    printf("Outside the range\n");  // Then, print "Outside the range" on the screen
+  }                                 // Close Condition Statement
+}                                   // Terminate a program
